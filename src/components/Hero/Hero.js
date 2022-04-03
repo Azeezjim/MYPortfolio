f4b63b77f1ab8ecee4b0efd4523b858bbe08f3bc
Azeezjim <azeexjim@gmail.com>
2022-04-03 03:01:18 -1100

    latest commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -25,7 +25,7 @@ const Hero = (props) => (
       </SectionText>
       <Button
         onClick={() =>
-          (window.location = "https://drive.google.com/drive/folders/1mVSG96HbJKMO2ULP8XtiUt5qZpC7Qa0j")
+          (window.location = "https://drive.google.com/file/d/1HJp0c9n4HUd5A1_hmZWF2-iJa08keTKV/view?usp=sharing")
         }
       >
         Learn More
